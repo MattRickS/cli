a80cd67edbd3823edb2ad06398f8c47c65ffc225
Matthew <mattalexshaw@gmail.com>
2019-07-21 16:21:27 +0100

    add colour options, docstrings

--- python/cli/actions.py
+++ python/cli/actions.py
@@ -4,6 +4,20 @@ import os
 
 
 def chunks_action(n):
+    """
+    Examples:
+        >>> parser = argparse.ArgumentParser()
+        >>> parser.add_argument("chunks", action=chunks_action(3), type=int, nargs="+")
+        >>> args = parser.parse_args("0 1 2 3 4 5".split())
+        >>> args.chunks
+        # [[0, 1, 2], [3, 4, 5]]
+
+    Args:
+        n (int): Number of chunks to group the arguments into
+
+    Returns:
+        argparse.Action
+    """
     class ChunksAction(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
             if len(values) % n != 0:
@@ -15,22 +29,53 @@ def chunks_action(n):
     return ChunksAction
 
 
-def colour_action():
+def colour_action(alpha=True, integers=False):
+    """
+    Action which generates a 3 (rgb) or 4 (rgba) index list representing a colour
+
+    Examples:
+        >>> parser = argparse.ArgumentParser()
+        >>> parser.add_argument("colour", action=colour_action(), type=float, nargs="+")
+        >>> args = parser.parse_args("0")
+        >>> args.colour
+        # [0.0, 0.0, 0.0, 1.0]
+        >>> args = parser.parse_args("0.1 0.2 0.3".split())
+        >>> args.colour
+        # [0.1, 0.2, 0.3, 1.0]
+
+    Args:
+        alpha (bool): Whether or not to include an alpha value, ie, it returns a
+            3 or 4 index list
+        integers (bool): If True, colours are interpreted using 0-255 integer
+            values instead of 0.0-1.0 floats
+
+    Returns:
+        argparse.Action
+    """
+    min_, max_ = (0, 255) if integers else (0.0, 1.0)
+
     class ColourAction(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
             if len(values) == 1:
                 values *= 3
-                values.append(1.0)
+                values.append(max_)
             elif len(values) == 3:
-                values.append(1.0)
-            elif len(values) != 4:
+                values.append(max_)
+            elif alpha and len(values) != 4:
                 parser.error(
-                    "Invalid number of arguments for colour, must be 1 (constant), "
-                    "3 (rgb) or 4 (rgba)"
+                    "Invalid number of arguments for {}, must be 1 (constant), "
+                    "3 (rgb) or 4 (rgba)".format(self.dest)
+                )
+            else:
+                parser.error(
+                    "Invalid number of arguments for {}, must be 1 (constant) "
+                    "or 3 (rgb)".format(self.dest)
                 )
 
-            if not all(0.0 <= v <= 1.0 for v in values):
-                parser.error("Colour values must be between 0 and 1")
+            if not all(min_ <= v <= max_ for v in values):
+                parser.error(
+                    "Colour values must be between {} and {}".format(min_, max_)
+                )
 
             setattr(args, self.dest, values)
 
@@ -38,6 +83,22 @@ def colour_action():
 
 
 def filepath_action(exists=False):
+    """
+    Action which ensures the filepath is absolute
+
+    Usage:
+        >>> parser = argparse.ArgumentParser()
+        >>> parser.add_argument("path", action=filepath_action())
+        >>> args = parser.parse_args(".")
+        >>> args.path
+        # /current/directory
+
+    Keyword Args:
+        exists (bool): If True, parser will error if the filepath does not exist
+
+    Returns:
+        argparse.Action
+    """
     class FilepathAction(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
             values = os.path.abspath(values)
